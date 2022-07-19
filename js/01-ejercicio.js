ecc98398fc0068c98344e7227fee42d2202a6a34
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2022-07-19 10:15:12 -0300

    ejercicio numero 1 realizado

--- js/01-ejercicio.js
+++ js/01-ejercicio.js
@@ -1,1 +1,10 @@
 //Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla en forma de lista  los doce nombres del arreglo.
+
+let meses = ['Enero', 'Febrero', 'Marzo', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre',' Octubre', 'Noviembre', 'Diciembre'];
+document.write('<h1>Meses del año <h1>')
+document.write('<ul>');
+for(let mesesAño = 0; mesesAño < meses.length; mesesAño++){
+    document.write('<li>' + meses [mesesAño] + '</li>')}
+    document.write('</ul>')
+
+
