86329a55fa5768a501f1d3d810cf76b26b6fa9f8
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2022-07-24 18:12:03 -0300

    ejercicio 3 y 4 de funciones realizados

--- js/Funciones/03-funciones.js
+++ js/Funciones/03-funciones.js
@@ -1,3 +1,17 @@
-// Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo, luego crear una función para calcular su perímetro y mostrarlo por pantalla.
+// Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo, 
+//luego crear una función para calcular su perímetro y mostrarlo por pantalla.
 
 // La fórmula del perímetro  es p = 2*(a +b)
+
+let lado1 = parseInt(prompt ('Ingrese el valor de uno de los lados de un rectangulo'))
+let lado2 = parseInt(prompt ('Ingrese un segundo valor de los lados de un rectangulo'))
+
+let resultadoFinal = perimetro (lado1,lado2)
+document.write ('El perimero del rectagulo es: ' + resultadoFinal)
+
+
+ function perimetro (a,b){
+  let resultado = 2* (a + b)
+  return (resultado)
+ }
+
--- js/Funciones/04-funciones.js
+++ js/Funciones/04-funciones.js
@@ -1,1 +1,19 @@
-//Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
+//Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, 
+//la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
+
+
+let numeroIngresado = tabla(parseInt( prompt('Ingrese un numero que desea multiplicar')))
+function tabla (numero){
+     document.write (`<table> 
+     <tbody> 
+     `)
+     for(i=1; i<=10; i++ ){
+        let resultado = numero*i
+        document.write(`<tr>`)
+
+         document.write (`<td> ${resultado}
+         </td>`)
+     }
+     document.write (` </tbody> </table> 
+     `)
+}
