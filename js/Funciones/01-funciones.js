3c3f2973e2f72c2ad38ee0ae318c7b745d1b38eb
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2022-07-24 17:32:34 -0300

    ejercicio numero 1 y 2 realizados

--- js/Funciones/01-funciones.js
+++ js/Funciones/01-funciones.js
@@ -1,1 +1,18 @@
-//Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
+//Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar.
+// Mostrar por pantalla el resultado devuelto por la función.
+
+
+let numeroIngresado = numeroEntero( parseInt(prompt('Ingrese un numero')))
+
+
+
+
+
+function numeroEntero (numero) {
+    if(numero% 2 ===0){document.write('Su numero es par')
+}else(document.write('Su numero es impar'))
+
+
+    
+}
+
--- js/Funciones/02-funciones.js
+++ js/Funciones/02-funciones.js
@@ -1,1 +1,15 @@
-//Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
+//Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. 
+//A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
+
+let parrafo = tiposDeLetra(prompt('Ingrese un texto'))
+
+
+
+function tiposDeLetra(texto) {
+    if(texto === texto.toLowerCase()){
+        document.write ('El texto esta escrito con minuscula')
+    }else if (texto === texto.toUpperCase()){
+        document.write ('Su texto esta escrito con mayuscula')
+    }else (document.write ('El texto esta escrito con mayusculas y minusculas'))
+    
+}
